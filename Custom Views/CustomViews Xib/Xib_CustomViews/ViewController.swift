0c7635003c3a330ffe2822df630f7bfda29e4b26
MAKAR4IK <eri.makar@yandex.ru>
2021-02-01 17:35:44 +0300

    ADDED: Custom_view_hometask

---
+++ Custom Views/CustomViews Xib/Xib_CustomViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xib_CustomViews
+//
+//  Created by Ann Yank on 23.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
